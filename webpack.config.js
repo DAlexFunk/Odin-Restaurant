ab3decc6a89df310200fac49384689b41b5490bd
Alex Funk <alexdagreat04@gmail.com>
2024-09-20 13:06:09 -0400

    Added stuff that will be the same for each page
    This includes the header and background

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,12 @@ module.exports = {
         {
             test: /\.css$/i,
             use: ["style-loader", "css-loader"],
-        }
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+  
+        },
     ],
   },
 };
